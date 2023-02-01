d104dda7f6c3d90e072c92063201e5b39a4cfc0e
NguyenThanhDuong <thanhduong2001@icloud.com>
2023-02-01 12:28:24 +0700

    Add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_092311) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_051210) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
